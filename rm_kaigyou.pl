dc28548f506194ac20a738b0d874483e1d9b6826
yoshiaki <kinko912000@gmail.com>
2014-02-05 17:49:54 +0900

    進んだ

--- rm_kaigyou.pl
+++ rm_kaigyou.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use Encode 'decode';
+use Encode 'encode';
 use warnings;
 use 5.10.0;
 use utf8;
@@ -7,7 +9,9 @@ open my $fh ,'<','story.txt';
 open LOG , ">edit.txt";
 
 while(my $line = readline $fh){
-    $line =~ s/。/\\/;
+    #  $line = decode('UTF-8',$line);
+    $line =~ s/\x{e38082}/\n/;
+    #  $line = encode('UTF-8',$line);
     print LOG "$line";
 }
 
